31928885e07dee96269c391a309735404f34921c
Your Name <you@example.com>
2024-12-27 15:23:06 +0500

    v2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: true,
+    },
+  },
 })
